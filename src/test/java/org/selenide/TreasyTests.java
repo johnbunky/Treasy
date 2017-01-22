f6cd607ea3d0ced1a5647e8c09f0bd469a4efa86
johnbunky <johnbunky@mail.ru>
2017-01-22 17:57:11 +0200

    add asserts

--- src/test/java/org/selenide/TreasyTests.java
+++ src/test/java/org/selenide/TreasyTests.java
@@ -1,28 +1,16 @@
 package org.selenide;
-import com.codeborne.selenide.SelenideElement;
-import org.bouncycastle.jcajce.provider.asymmetric.rsa.CipherSpi;
-import org.junit.Before;
-import org.openqa.selenium.By;
 
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import static com.codeborne.selenide.Condition.disappears;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.value;
+import static com.codeborne.selenide.Configuration.startMaximized;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.byTitle;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.switchTo;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
 
 
 /**
@@ -30,13 +18,42 @@ import static org.junit.Assert.assertThat;
  */
 public class TreasyTests {
 
+    private String BASE_URL = "https://treasy-tst.eu-gb.mybluemix.net/#/start";
+
+    @BeforeClass
+    public static void openInbox() {
+
+        startMaximized = false;
+
+    }
+
+    @AfterClass
+    public static void logout() {
+        closeWebDriver();
+    }
+
     @Test
     public void registerWithActivationEmailAndConfirmationLink(){
 
-        open("https://treasy-tst.eu-gb.mybluemix.net/#/start");
+        //arrange
+        open(BASE_URL);
+        //act
         inputEmailAddress();
         openActivationEmail();
-        $(withText("https://treasy-tst.eu-gb.mybluemix.net/v1/user/confirm/")).shouldBe();
+        openConfirmationLink();
+        switchTo().defaultContent();
+        open(BASE_URL);
+        //assert
+        $("input[placeholder=\"Select drug\"]").shouldBe();
+        $("*[placeholder=\"Dose\"]").shouldBe();
+        $("input[placeholder=\"Animal name\"]").shouldBe();
+        $("input[placeholder=\"Select animal type\"]").shouldBe();
+        $(byText("Save")).shouldBe();
+
+    }
+
+    private void openConfirmationLink() {
+        $("a[rel=\"noreferrer\"]").click();
     }
 
     private void openActivationEmail() {
@@ -49,7 +66,7 @@ public class TreasyTests {
     }
 
     private void inputEmailAddress() {
-        $("input[type=\"email\"]").setValue("treasy.uitest@gmail.com");
+        $("input[type=\"email\"]").val("treasy.uitest@gmail.com");
         $(byText("Request confirmation link")).click();
     }
 
