fb2ee3de27b044e80eab3d19c7f3aa618984b56a
johnbunky <johnbunky@mail.ru>
2017-01-25 19:30:51 +0200

    assert changed

--- src/test/java/org/selenide/TreasyTests.java
+++ src/test/java/org/selenide/TreasyTests.java
@@ -3,13 +3,16 @@ package org.selenide;
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.*;
+import com.codeborne.selenide.Configuration;
+
 
 import static com.codeborne.selenide.Condition.disappears;
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Configuration.startMaximized;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.switchTo;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
 
@@ -18,13 +21,12 @@ import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
  */
 public class TreasyTests {
 
-    private String BASE_URL = "https://treasy-tst.eu-gb.mybluemix.net/#/start";
+    private String BASE_URL = "https://treasy-tst.eu-gb.mybluemix.net";
 
     @BeforeClass
     public static void openInbox() {
-
         startMaximized = false;
-
+        //Configuration.browser = "chrome";
     }
 
     @AfterClass
@@ -33,41 +35,39 @@ public class TreasyTests {
     }
 
     @Test
-    public void registerWithActivationEmailAndConfirmationLink(){
+    public void registerWithActivationEmailAndConfirmationLink() {
 
-        //arrange
+        // Arrange
         open(BASE_URL);
-        //act
+
+        // Act
         inputEmailAddress();
         openActivationEmail();
-        openConfirmationLink();
-        switchTo().defaultContent();
-        open(BASE_URL);
-        //assert
-        $("input[placeholder=\"Select drug\"]").shouldBe();
-        $("*[placeholder=\"Dose\"]").shouldBe();
-        $("input[placeholder=\"Animal name\"]").shouldBe();
-        $("input[placeholder=\"Select animal type\"]").shouldBe();
-        $(byText("Save")).shouldBe();
+        openConfirmationLink();;
 
+        // Assert
+        $(byText("Congratulations, you have successfully registered to Treasy.")).should(exist);
     }
 
     private void openConfirmationLink() {
         $("a[rel=\"noreferrer\"]").click();
+        switchTo().defaultContent(); // Switch to default window
+        sleep(10000);
     }
 
     private void openActivationEmail() {
+        $("body").sendKeys(Keys.CONTROL + "t"); // Open empty tab
         open("treasy.uitest@gmail.com");
         $("#Email").val("treasy.uitest@gmail.com").pressEnter();
         $("#Passwd").val("N3cqNkjF6RvN");
         $("#signIn").click();
         $(".error-msg").waitUntil(disappears, 2000);
         $(byText("One more click to Treasy!")).click();
+        $("body").sendKeys(Keys.CONTROL + "\t"); // Switch to previous tab
     }
 
     private void inputEmailAddress() {
         $("input[type=\"email\"]").val("treasy.uitest@gmail.com");
         $(byText("Request confirmation link")).click();
     }
-
 }
