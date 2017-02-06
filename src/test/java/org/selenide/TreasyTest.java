ad51e8ee6fd275c4889bf91f375c78007b7f3500
Leo Felber <leo.felber@xinventa.ch>
2017-02-07 00:24:27 +0100

    Refactoring

--- src/test/java/org/selenide/TreasyTest.java
+++ src/test/java/org/selenide/TreasyTest.java
@@ -1,18 +1,35 @@
 package org.selenide;
 
-import org.junit.*;
-import org.openqa.selenium.By;
-
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Configuration.*;
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.disappears;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.not;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Configuration.browser;
+import static com.codeborne.selenide.Configuration.holdBrowserOpen;
+import static com.codeborne.selenide.Configuration.startMaximized;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.refresh;
+import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.switchTo;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
 
 /**
  * Created by johnbunky on 27.01.17.
  */
 public class TreasyTest {
+
+    private static final long FIVE_SECONDS = 5000;
+
+   private static final long TWO_SECONDS = 2000;
 
     public static String BASE_URL = "https://treasy-tst.eu-gb.mybluemix.net";
 
@@ -33,6 +50,9 @@ public class TreasyTest {
 
         // Arrange
         open(BASE_URL);
+        
+        // Wait for UI to load the dynamic parts
+        sleep(FIVE_SECONDS);
 
         // Act
         inputEmailAddress();
@@ -44,14 +64,14 @@ public class TreasyTest {
     public void registerWithActivationEmailAndConfirmationLink() {
 
         // Assert
-        $(byText("Congratulations, you have successfully registered to Treasy.")).should(exist);
+       switchTo().window("qikCloud - Treasy");
+        $(byText("Anmeldung erfolgreich")).should(exist);
     }
 
     @Test
     public void checkSettingsListAnimalType() {
 
-        // Arrange
-        open(BASE_URL + "/#/welcome");
+       switchTo().window("Treasy");
 
         // Act
         openSettingPage();
@@ -67,8 +87,7 @@ public class TreasyTest {
     @Test
     public void  checkDeletingFromListAnimalType(){
 
-        // Arrange
-        open(BASE_URL + "/#/welcome");
+       switchTo().window("Treasy");
 
         // Act
         openSettingPage();
@@ -101,28 +120,32 @@ public class TreasyTest {
 
     private void openSettingPage() {
         $(By.xpath("//*[@id='menu']")).click();
-        $(byText("Settings")).click();
+        $(byText("Einstellungen")).click();
         sleep(2000);
     }
 
     private void openConfirmationLink() {
         $(byText("https://treasy-tst.eu-gb.")).click();
-        switchTo().window(0);
-        sleep(60000);
+        switchTo().window("Treasy");
+        $(byText("Schliessen")).click();
+        $(byText("Start")).click();
     }
 
     private void openActivationEmail() {
+       // Wait for activation email to arrive
+       sleep(FIVE_SECONDS);
+       
         executeJavaScript("window.open('https://mail.google.com');"); // Open a new tab
-        switchTo().window(1);
+        switchTo().window("Gmail");
         $("#Email").val("treasy.uitest@gmail.com").pressEnter();
         $("#Passwd").val("N3cqNkjF6RvN");
         $("#signIn").click();
         $(".error-msg").waitUntil(disappears, 2000);
-        $(byText("TST: One more click to Treasy!")).click();
+        $(byText("TST: Noch ein Klick bis Treasy!")).click();
     }
 
     private void inputEmailAddress() {
         $("input[type=\"email\"]").val("treasy.uitest@gmail.com");
-        $(byText("Request confirmation link")).click();
+        $(byText("Bestätigungslink anfordern")).click();
     }
 }
